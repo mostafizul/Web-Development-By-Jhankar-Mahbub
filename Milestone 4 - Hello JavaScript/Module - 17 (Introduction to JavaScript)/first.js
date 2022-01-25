e9977a80396f476eb3eb87e40c3edb1a468f620d
mostafizul <siamahmed365@gmail.com>
2022-01-26 01:26:03 +0600

    Module - 17

---
+++ Milestone 4 - Hello JavaScript/Module - 17 (Introduction to JavaScript)/first.js
@@ -0,0 +1,3 @@
+var a = prompt("enter a number");
+
+document.write("Number is "+ a);
---
+++ Milestone 4 - Hello JavaScript/Module - 17 (Introduction to JavaScript)/variable.js
@@ -0,0 +1,9 @@
+var firstName = "siam";
+document.write(firstName+"<br>");
+
+var age = 27;
+document.write(age+"<br>");
+
+var boolean = true;
+document.write(boolean+"<br>");
+
