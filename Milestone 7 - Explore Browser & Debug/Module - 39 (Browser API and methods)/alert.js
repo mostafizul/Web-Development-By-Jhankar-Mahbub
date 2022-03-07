27076066e39a0df8715e9fecfb8fcfbec0cafa84
mostafizul <siamahmed365@gmail.com>
2022-03-08 00:54:26 +0600

    Module 39

---
+++ Milestone 7 - Explore Browser & Debug/Module - 39 (Browser API and methods)/alert.js
@@ -0,0 +1,14 @@
+const myFunction = () =>{
+    alert("alert");
+}
+
+const confirmFunction = () =>{
+   const response = confirm("picnic e jabi?");
+   console.log(response);
+}
+
+const myName = () =>{
+    const name = prompt("what is your name: ");
+    console.log(name);
+
+}
