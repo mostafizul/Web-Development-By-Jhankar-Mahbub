8a29d11e972ec8d08e1c6b5bc57951c37760cf6b
mostafizul <siamahmed365@gmail.com>
2022-03-16 15:59:45 +0600

    Module 45

--- Milestone 8 - Simple React/Module - 45 (React Core Concepts, JSX, props, state)/reactjs-core-concepts-recap/src/App.js
+++ Milestone 8 - Simple React/Module - 45 (React Core Concepts, JSX, props, state)/reactjs-core-concepts-recap/src/App.js
@@ -1,27 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  const products = [
+  /* const products = [
     {name:'laptop',price:53000},
     {name:'Iphone',price:40000},
     {name:'watch',price:2000},
     {name:'tablet',price:10000},
 
-]
+] */
+
   return (
     <div className="App">
-      {
-        products.map(product =><Product name={product.name} price={product.price}></Product>)
-      }
-     {/*  <Product name=" laptop" price="75000"></Product>
-      <Product name=" Iphone" price="50000"></Product>
-      <Product name=" watch" price="20000"></Product> */}
+       <Counter></Counter>
     </div>
   );
 }
 
-function Product(props){
+function Counter(){
+  const [count, setCount] = useState(55);
+  const increaseCount = () => setCount(count+1);
+  const decreaseCount = () => setCount(count-1)
+  return(
+    <div>
+      <h1>Count:{count} </h1>
+      <button onClick={increaseCount}>Increase</button>
+      <button onClick={decreaseCount}>Decrease</button>
+    </div>
+  )
+}
+
+/* function Product(props){
   return(
     <div className='productname'>
       <h3>Name:{props.name} </h3>
@@ -29,6 +39,13 @@ function Product(props){
     </div>
 
   )
-}
+} */
 
 export default App;
+
+/* {
+  products.map(product =><Product name={product.name} price={product.price}></Product>)
+} */
+{/*  <Product name=" laptop" price="75000"></Product>
+<Product name=" Iphone" price="50000"></Product>
+<Product name=" watch" price="20000"></Product> */}
