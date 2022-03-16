fc153b12861520f2ab39a862d3c4f9dab600759d
mostafizul <siamahmed365@gmail.com>
2022-03-16 19:25:58 +0600

    Module 45

--- Milestone 8 - Simple React/Module - 45 (React Core Concepts, JSX, props, state)/reactjs-core-concepts-recap/src/App.js
+++ Milestone 8 - Simple React/Module - 45 (React Core Concepts, JSX, props, state)/reactjs-core-concepts-recap/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   /* const products = [
@@ -13,9 +13,39 @@ function App() {
 
   return (
     <div className="App">
-       <Counter></Counter>
+       {/* <Counter></Counter> */}
+
+       <ExternalUsers></ExternalUsers>
     </div>
   );
+}
+
+function ExternalUsers (){
+  const [users, setUser] = useState([]);
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res=>res.json())
+    .then(data=>setUser(data));
+  },[])
+  return(
+  <div>
+    <h2>External Users</h2>
+    <p>{users.length}</p>
+    {
+      users.map(user=><User name={user.name} email={user.email}></User>)
+    }
+  </div>
+
+  )
+}
+
+function User(props){
+  return(
+    <div style={{border:'1px solid red',margin:'10px'}}>
+      <h3>Name:{props.name}</h3>
+      <p>Email:{props.email}</p>
+    </div>
+  )
 }
 
 function Counter(){
