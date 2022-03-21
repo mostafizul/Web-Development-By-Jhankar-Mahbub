834f968fe64c2de8a33f8e51dfec59b70dcb9cde
mostafizul <siamahmed365@gmail.com>
2022-03-21 23:47:16 +0600

    Module 46.5

---
+++ Milestone 8 - Simple React/Module - 46.5 (React Core Concept Recap and Practice Day)/reactjs-core-concepts-recap/src/App.js
@@ -0,0 +1,57 @@
+import logo from './logo.svg';
+import './App.css';
+import {useEffect, useState} from 'react';
+function App() {
+  return (
+    <div className="App">
+      <LoadPosts></LoadPosts>
+      <District name="Dhaka"></District>
+      <District name="rangamati"></District>
+    </div>
+  );
+}
+
+function LoadPosts(){
+  const [posts, setPost] = useState([]);
+  useEffect(() =>{
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res=>res.json())
+    .then(data=>setPost(data));
+     
+  },[])
+   return(
+     <div>
+       <h1>Posts: {posts.length}</h1>
+       {
+         posts.map(post=><ShowPost title={post.title}></ShowPost>)
+       }
+     </div>
+   )
+}
+function ShowPost(props){
+  return(
+    <div>
+      <h2>Title:{props.title}</h2>
+    </div>
+  )
+}
+function District(props){
+  const [power,setPower] = useState(1);
+  const boostPower = () =>{
+    const newPower = power*2;
+    setPower(newPower);
+  }
+  const unBoostPower = () =>{
+    const newPower = power-2;
+    setPower(newPower);
+  }
+  return(
+  <div>
+    <h2>Name:{props.name}</h2>
+    <h4>Power:{power}</h4>
+    <button onClick={boostPower}>Boost</button>
+    <button onClick={unBoostPower}>Unboost</button>
+  </div>
+  )
+}
+export default App;
